7c32c2ffc2d17882e84e871c8b94e2607469ba98
Giri Prasad S <51823589+giri-webcodes@users.noreply.github.com>
2021-05-01 20:27:41 +0530

    Add files via upload

---
+++ expensetype.js
@@ -0,0 +1,13 @@
+var expenseTypes=[
+    "Vegetables",
+    "Fruits",
+    "Milk",
+    "Curd",
+    "Stationary",
+    "Mobile Phone",
+    "Mobile Recharge",
+    "Gas Cylinder",
+    "Rice Bag",
+    "Kid",
+    "Amazon"
+];
