eead836943b802668a3d426f156136fc1b3dbe60
Giri Prasad S <51823589+giri-webcodes@users.noreply.github.com>
2021-05-05 20:28:05 +0530

    Update expensetracker.js

--- expensetracker.js
+++ expensetracker.js
@@ -1,11 +1,11 @@
 var app = angular.module('expenseApp',[]);
 
-app.controller('ExpenseTrackerController',['$scope','$window',function($scope,$window){
+app.controller('ExpenseTrackerController',['$scope','$window','$filter',function($scope,$window,$filter){
 
 //initialize $scope
  $scope.list=[];
  $scope.expenseMonth=getExpenseMonth();
- $scope.masterTemplate="master.html";
+ var editEntity=null;
 
  if(localStorage.getItem("list") !== null)
  {
@@ -18,10 +18,10 @@ $scope.Add=function(){
 $scope.list.push({id:($scope.list.length +1),expense:$scope.newExpense,amount:parseFloat($scope.amount),date:new Date($scope.date),comment:$scope.comment});
 localStorage.setItem("list",JSON.stringify($scope.list));
 $scope.newExpense='';
-$scope.amount=0.0;
+$scope.amount='';
 $scope.date=new Date();
 $scope.comment='';
-document.getElementById("savealert").style.display=null;
+document.getElementById("alert").style.display=null;
 };
 
 $scope.resetForm = function()
@@ -53,25 +53,56 @@ $scope.getExpense=function()
 {
     var jsonse = JSON.stringify($scope.list);
     var blob = new Blob([jsonse], {
-      type: "application/json"
+      type: "application/json"  
     });
     $scope.filename = "expense_2021.json";
     saveAs(blob, $scope.filename);
 };
 
-$scope.delete=function(item)
+$scope.delete=function()
 {
  if($window.confirm('Delete expense?'))
  {
-  var index= $scope.list.indexOf(item);
+  var index= $scope.list.indexOf(editEntity);
   $scope.list.splice(index,1);
-  $window.alert('Expense deleted');
+  $('#alert').show();
+  $('#alert').html('<strong>Successful:</strong> Expense deleted');
   localStorage.clear();
   localStorage.setItem("list",JSON.stringify($scope.list));
  }
 };
 
+//edit function
+$scope.editPage=function(id){
+  $window.location.href="edit.html?id="+id;
+};
+
+$scope.loadEditData=function()
+{
+  const params = new URLSearchParams(location.search);
+  var id= params.get('id');
+  var item= $filter('filter')($scope.list, {'id':id});
+ if(item.length){
+  editEntity=item[0];
+ $scope.newExpense=item[0].expense;
+$scope.amount=item[0].amount;
+$scope.date=new Date(item[0].date);
+$scope.comment=item[0].comment;
+ }
+};
+
+$scope.update=function(){
+var index=  $scope.list.indexOf(editEntity);
+$scope.list.splice(index,1);
+$scope.list.push({id:editEntity.id,expense:$scope.newExpense,amount:parseFloat($scope.amount),date:new Date($scope.date),comment:$scope.comment});
+localStorage.clear();
+localStorage.setItem("list",JSON.stringify($scope.list));
+document.getElementById("alert").style.display=null;
+}
+
 }]);
+
+
 
 
 
@@ -115,4 +146,6 @@ function getExpenseMonth()
 
  return months[date.getMonth()] + ' ' + date.getFullYear();
 }
+
+
 
